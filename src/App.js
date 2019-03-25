b0b2497c9f77a0bed7a028b4fc4752ca6fdf8885
Vijay Solanki <40413513+solankivj94@users.noreply.github.com>
2019-03-25 23:17:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 		return (
 			<React.Fragment>
 				<h1 className="main-heading">Custom React Video Player</h1>
-				<Player videoSrc={myVideo} />
+				<Player videoSrc="https://clips.vorwaerts-gmbh.de/VfE_html5.mp4" />
 			</React.Fragment>
 		);
 	}
