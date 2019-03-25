133bb6ae064d7d46c8b7ece2a00de97ef72f1fae
Vijay Solanki <40413513+solankivj94@users.noreply.github.com>
2019-03-25 23:26:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 			<React.Fragment>
 				<h1 className="main-heading">Custom React Video Player</h1>
 				<Player videoSrc="https://clips.vorwaerts-gmbh.de/VfE_html5.mp4" />
+			<h1 className="main-heading">Please Check on Chrome, Currently working on cross browser support  👷🏻</h1>
 			</React.Fragment>
 		);
 	}
